81b8588505f51b588e201051267cf85d98eadb10
Sandy <60555203+sfroonjian@users.noreply.github.com>
2020-03-14 17:56:29 -0400

    added comments to main.py files

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,14 +11,19 @@ change = []
 with open(csvpath) as csvfile:
 	csvreader = csv.reader(csvfile, delimiter = ',')
 	csv_header = next(csvreader)
+	# goes through every row in the data set
 	for row in csvreader:
+		# add each item in the date column to the date list
 		date.append(row[0])
+		# add each item in the profit/loss column to the profloss list
 		profloss.append(int(row[1]))
 
+# goes through the length of the profloss list, subtracts the current value from the next value, and adds that value to the change list
 for i in range(len(profloss) -1):
 	num = (profloss[i+1]) - (profloss[i])
 	change.append(int(num))
 
+# uses built-in functions to find the values of needed variables
 totalmonths = len(date)
 totalprof = sum(profloss)
 lenchange = len(change)
@@ -27,11 +32,16 @@ average = round((totalchange / lenchange), 2)
 maxchange = max(change)
 minchange = min (change)
 
+# finds the position in the change list of the max amount
 maxposition = change.index(maxchange)
+# finds the position in the change list of the min amount
 minposition = change.index(minchange)
+# matches up the date of the max change by matching indexes
 datemax = date[maxposition + 1]
+# matches up the date of the min change by matching indexes
 datemin = date[minposition + 1]
 
+# writes data to a csv file
 with open(output_path, 'w') as outputfile:
 	writer = csv.writer(outputfile)
 	writer.writerow(["Financial Analysis"])
@@ -41,6 +51,7 @@ with open(output_path, 'w') as outputfile:
 	writer.writerow(["Greatest Increase in Profits:", datemax, maxchange])
 	writer.writerow(["Greatest Increase in Profits:", datemin, minchange])
 
+# prints data to terminal
 print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(totalmonths))
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,15 +5,19 @@ csvpath = os.path.join("election_data.csv")
 output_path = os.path.join("output.csv")
 
 voterid = []
-country = []
+county = []
 candidate = []
 
 with open(csvpath) as csvfile:
 	csvreader = csv.reader(csvfile, delimiter = ',')
 	csv_header = next(csvreader)
+	# goes through every row of the data file
 	for row in csvreader:
+		# adds each item from the voter id column to the voterid list
 		voterid.append(row[0])
-		country.append(row[1])
+		# adds each item from the county column to the county list
+		county.append(row[1])
+		# adds each item from the canditate column to the candidate list
 		candidate.append(row[2])
 
 khan = 0
@@ -21,6 +25,7 @@ correy = 0
 li = 0
 otooley = 0
 
+# goes through the candidate list and adds one for each candidate if they were voted for
 for i in range(len(candidate)):
 	if candidate[i] == "Khan":
 		khan += 1
@@ -31,6 +36,7 @@ for i in range(len(candidate)):
 	elif candidate[i] == "O'Tooley":
 		otooley += 1
 
+# compares the number of votes each candidate recevied, to see who the winner is
 if (khan > correy) and (khan > li) and (khan > otooley):
 	winner = "Khan"
 elif (correy > khan) and (correy > li) and (correy > otooley):
@@ -40,12 +46,14 @@ elif (li > correy) and (li > khan) and (li > otooley):
 else:
 	winner = "O'Tooley"
 
+# finds the total amount of votes and the percent of votes each candidate recevied
 totalvotes = len(voterid)
 kpercent = round(((khan / totalvotes) * 100), 3)
 cpercent = round(((correy / totalvotes) * 100), 3)
 lpercent = round(((li / totalvotes) * 100), 3)
 opercent = round(((otooley / totalvotes) * 100), 3)
 
+# writes data for the the csv file
 with open(output_path, 'w') as outputfile:
 	writer = csv.writer(outputfile)
 	writer.writerow(["Election Results"])
@@ -56,6 +64,7 @@ with open(output_path, 'w') as outputfile:
 	writer.writerow(["O'Tooley:", opercent, otooley])
 	writer.writerow(["Winner:", winner])
 
+# prints data to terminal
 print("Election Results")
 print("----------------------------")
 print("Total Votes: " + str(totalvotes))
